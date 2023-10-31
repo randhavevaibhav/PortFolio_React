849d00ba25c28f85a1d2b6a776c842be300f365d
Vaibhav Randhave <randhavevaibhav3@gmail.com>
2023-10-31 20:51:39 +0530

    removed commented code

--- src/Components/SuccessBox.js
+++ src/Components/SuccessBox.js
@@ -1,25 +1,15 @@
 import React from "react";
-import "../Styles/SuccessBox.css"
+import "../Styles/SuccessBox.css";
 function SuccessBox() {
+  setTimeout(() => {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+    document
+      .getElementById("dismiss-popup-btn")
+      .addEventListener("click", function () {
+        document.getElementsByClassName("popup")[0].classList.remove("active");
+      });
+  }, 500);
 
-    setTimeout(()=>{
-        // document.getElementById("open-popup-btn").addEventListener("click",function(){
-        //     document.getElementById("open-popup-btn").style.display = "none";
-        //     document.getElementsByClassName("popup")[0].classList.add("active");
-        //   });
-           
-        //   document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
-        //     document.getElementById("open-popup-btn").style.display = "block";
-        //     document.getElementsByClassName("popup")[0].classList.remove("active");
-        //   });
-        // document.getElementById("open-popup-btn").style.display = "none";
-        document.getElementsByClassName("popup")[0].classList.add("active");
-         document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
-            // document.getElementById("open-popup-btn").style.display = "block";
-            document.getElementsByClassName("popup")[0].classList.remove("active");
-          });
-    },500)
-    
   return (
     <>
       <div className="popup center">
@@ -27,18 +17,11 @@ function SuccessBox() {
           <i className="fa fa-check"></i>
         </div>
         <div className="title">Success!!</div>
-        <div className="description">
-          Email Sent successfully !!!
-        </div>
+        <div className="description">Email Sent successfully !!!</div>
         <div className="dismiss-btn">
-          <button id="dismiss-popup-btn" >Dismiss</button>
-        
+          <button id="dismiss-popup-btn">Dismiss</button>
         </div>
       </div>
-      {/* <div className="bottom-right">
-        <button id="open-popup-btn" >Open Popup</button>
-     
-      </div> */}
     </>
   );
 }
