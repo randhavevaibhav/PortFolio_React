0482faf32d929d2fbaf0d527168609315198e67c
Vaibhav Randhave <randhavevaibhav3@gmail.com>
2023-11-02 20:42:00 +0530

    Added lottile animation

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,41 +7,52 @@ function About() {
     <h1 className="text-center"  id="navAbout" >About Me</h1>
     <div className="row mt-4">
       <div className="col-lg-6">
-        <img src="images/about.jpeg" className="image_about" alt="" />
+      <dotlottie-player
+      className="lottie-image-01"
+  src="https://lottie.host/8b1638b2-dd01-4968-b6d9-020659eacf86/K7eqADpRyh.lottie"
+  background="transparent"
+  speed={1}
+  style={{ 
+  }}
+  loop=""
+  autoPlay=""
+/>
+
       </div>
       <div className="col-lg-6">
         <p>
           {" "}
-          Designing and Coding Rules and Coding Rule sets Preparing Alert Workflows and Routing Policies Working on VTL files, managing back-end data with Oracle SQL Testing alert workflows with selenium java BDD framework.
+          Hello, I’m Vaibhav, a web developer.
+
+I’ve been working as a web developer for a few years now, and I’m passionate about everything that goes into making websites work well and look beautiful. The web is my passion because it’s where I can find the most exciting projects to work on. It’s also where I get to express my creativity in ways that are hard to find elsewhere.
         </p>
+        
         <div className="row mt-3">
           <div className="col-md-6">
+          <p>Programming languages known :</p>
             <ul>
-              <li>Windsurfing</li>
-              <li>Mountain-climbing</li>
-              <li>Brain-surgery</li>
+              <li>C</li>
+              <li>C++</li>
+              <li>Java</li>
+              <li>JavaScript</li>
+              <li>Oracle SQL and PL/SQL</li>
+              <li>Python</li>
             </ul>
           </div>
           <div className="col-md-6">
+          <p>Frameworks/libraries:</p>
             <ul>
-              <li>Creative-writing</li>
-              <li>Gymnastics</li>
-              <li>Landscape painting</li>
+              <li>BDD Selenium Java (for automation testing)</li>
+              <li>React Js</li>
+              <li>Jersey (for REST API)</li>
+              <li>Pygame</li>
             </ul>
           </div>
         </div>
         <div className="row mt-3">
           <p>
             {" "}
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
+            My background in programming has given me an understanding of how the software works and why it works the way it does. This allows me to ensure your project is running smoothly, even if you’re not always able to see what’s going on under the hood.
           </p>
         </div>
       </div>
